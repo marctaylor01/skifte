665356d340bbe174ac74c75d829523bd0652ca42
Marc Taylor <marc@taylorit.com>
2023-04-29 16:29:13 +0200

    fix texts and images for postspecific

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ import PostSpecific from './routes/post/post';
 import data from './routes/post/postText.json'
 import webbImg from './images/webb.jpg'
 import ölBarImg from './images/öl&bar.jpg'
+import qImg from './images/chief.jpg'
+import kenImg from './images/cash.jpg'
+import werkImg from './images/werk.jpg'
+import biljettImg from './images/biljett&u-lag.jpg'
+import notFoundImg from './images/notFound.jpg'
 
 const App = () => (
   <HashRouter>
@@ -25,62 +30,62 @@ const App = () => (
       <Route
         exact
         path='/öl&bar'
-        element= {<PostSpecific title='Öl & Bar' text={data.webb} image={ölBarImg}/>}
+        element= {<PostSpecific title='Öl & Bar' text={data['öl&bar']} image={ölBarImg}/>}
       />
       <Route
         exact
         path='/q'
-        element= {<PostSpecific title='Webb & ÖD' text={data.webb}/>}
+        element= {<PostSpecific title='Chief' text={data.q} image={qImg}/>}
       />
       <Route
         exact
         path='/ken'
-        element= {<PostSpecific title='Webb & ÖD' text={data.webb}/>}
+        element= {<PostSpecific title='Cash' text={data.ken} image={kenImg}/>}
       />
       <Route
         exact
         path='/trykk-queen'
-        element= {<PostSpecific title='Trykk' text={data.webb} image={ölBarImg}/>}
+        element= {<PostSpecific title='Trykk' text={data['trykk-queen']} image={notFoundImg}/>}
       />
       <Route
         exact
         path='/trykk-king'
-        element= {<PostSpecific title='Öl & Bar' text={data.webb} image={ölBarImg}/>}
+        element= {<PostSpecific title='Trykk' text={data['trykk-king']} image={notFoundImg}/>}
       />
       <Route
         exact
         path='/mos'
-        element= {<PostSpecific title='Öl & Bar' text={data.webb} image={ölBarImg}/>}
+        element= {<PostSpecific title='MoS' text={data.mos} image={notFoundImg}/>}
       />
       <Route
         exact
         path='/prolk'
-        element= {<PostSpecific title='Öl & Bar' text={data.webb} image={ölBarImg}/>}
+        element= {<PostSpecific title='PR & Folk' text={data.prolk} image={notFoundImg}/>}
       />
       <Route
         exact
         path='/spons'
-        element= {<PostSpecific title='Öl & Bar' text={data.webb} image={ölBarImg}/>}
+        element= {<PostSpecific title='Spons' text={data.spons} image={notFoundImg}/>}
       />
       <Route
         exact
         path='/pr&event'
-        element= {<PostSpecific title='Öl & Bar' text={data.webb} image={ölBarImg}/>}
+        element= {<PostSpecific title='PR & Event' text={data.prevent} image={notFoundImg}/>}
       />
       <Route
         exact
         path='/biljett&u-lag'
-        element= {<PostSpecific title='Öl & Bar' text={data.webb} image={ölBarImg}/>}
+        element= {<PostSpecific title='Biljett & U-lag' text={data['biljett&u-lag']} image={biljettImg}/>}
       />
       <Route
         exact
         path='/werk'
-        element= {<PostSpecific title='Öl & Bar' text={data.webb} image={ölBarImg}/>}
+        element= {<PostSpecific title='Werk' text={data.werk} image={werkImg}/>}
       />
       <Route
         exact
         path='/j4'
-        element= {<PostSpecific title='Öl & Bar' text={data.webb} image={ölBarImg}/>}
+        element= {<PostSpecific title='J^8' text={data.j4} image={notFoundImg}/>}
       />
     </Routes>
   </HashRouter>
